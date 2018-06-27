b8ee6a63e0e0e838b8329bb1d2329142164d1898
Bruce <bruce.jh.hamilton@gmail.com>
2018-06-27 21:52:15 +1200

    Fix for media urls when served on github pages.

--- main.jsx
+++ main.jsx
@@ -1,15 +1,15 @@
 
 const tracks = [
-  { title: 'Camping in a Dungeon', url: '/media/camping-in-a-dungeon.mp3' },
-  { title: 'Exploring a Cave', url: '/media/exploring-a-cave.mp3' },
-  { title: 'Fighting Goblins', url: '/media/fighting-goblins.mp3' },
-  { title: 'Traveling in Forest', url: '/media/traveling-in-forest.mp3' },
-  { title: 'Camping in Forest', url: '/media/camping-in-forest.mp3' },
-  { title: 'Exploring a Crypt', url: '/media/exploring-a-crypt.mp3' },
-  { title: 'Fighting Orcs', url: '/media/fighting-orcs.mp3' },
-  { title: 'Chased in a Dungeon', url: '/media/chased-in-a-dungeon.mp3' },
-  { title: 'Fighting Beasts', url: '/media/fighting-beasts.mp3' },
-  { title: 'Shopping in a City Market', url: '/media/shopping-in-a-city-market.mp3' }
+  { title: 'Camping in a Dungeon', url: 'media/camping-in-a-dungeon.mp3' },
+  { title: 'Exploring a Cave', url: 'media/exploring-a-cave.mp3' },
+  { title: 'Fighting Goblins', url: 'media/fighting-goblins.mp3' },
+  { title: 'Traveling in Forest', url: 'media/traveling-in-forest.mp3' },
+  { title: 'Camping in Forest', url: 'media/camping-in-forest.mp3' },
+  { title: 'Exploring a Crypt', url: 'media/exploring-a-crypt.mp3' },
+  { title: 'Fighting Orcs', url: 'media/fighting-orcs.mp3' },
+  { title: 'Chased in a Dungeon', url: 'media/chased-in-a-dungeon.mp3' },
+  { title: 'Fighting Beasts', url: 'media/fighting-beasts.mp3' },
+  { title: 'Shopping in a City Market', url: 'media/shopping-in-a-city-market.mp3' }
 ];
 
 ReactDOM.render(
